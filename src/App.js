6235f951423b6c8ca88e584232d30c9c28a3bee1
wrjren <winnierjren@gmail.com>
2022-03-04 15:59:43 -0500

    added material ui and lineGraph

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
 import barChart from './Bar';
-import BarChart from './Bar';
+import LineGraph from './LineGraph';
+import React, { useEffect } from "react";
+import * as d3 from "d3";
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <BarChart/>
+      <div> Researcher Dashboard</div>
+      <div>
+        
+      </div>
+      <LineGraph/>
+      <LineGraph/>
     </div>
   );
 }
