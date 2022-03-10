52c48c23569ac9fb0e92b0a77a4ccdbb9ec029d7
wrjren <winnierjren@gmail.com>
2022-03-10 17:21:51 -0500

    data parsed but line does not draw

--- src/LineGraph.js
+++ src/LineGraph.js
@@ -1,19 +1,34 @@
 import React, { useEffect } from "react";
 import * as d3 from "d3";
+import { geoNaturalEarth1 } from "d3";
 
 function LineGraph() {
 
   const createGraph = async () => {
 
     // read data from csv and format variables
-    let data = await d3.csv('https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/3_TwoNumOrdered_comma.csv')
-    var parseTime = d3.timeParse("%Y-%m-%d");
-  
+    let data = await d3.csv('https://raw.githubusercontent.com/Het-Patel/vergo/main/dataprep/testData.sto?token=GHSAT0AAAAAABSHQPQ4IGBFQXPWQPEQBYMYYRTUYYA')
+    let data1 = await d3.csv('https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/3_TwoNumOrdered_comma.csv')
+    
+    let row = []
     data.forEach((d) => {
-      d.date = parseTime(d.date);
-      d.value = +d.value;
-    });
-    console.log(data)
+      d = JSON.stringify(d)
+      let tempData = d.split(":")[1]
+      // console.log(tempData)
+      tempData = tempData.replace(/[&#,+()$~%.'":*?<>{}]/g, '')
+      row.push(tempData)
+    })
+    let help = []
+    row.forEach((d)=> {
+      let tempData = {date: d.split("\\t")[0], value: d.split("\\t")[1]}
+      help.push(tempData)
+    })
+  
+    // data.forEach((d) => {
+    //   help.date = d3.tsvParse()
+    //   help.value = +help.value;
+    // });
+    // console.log(data)
 
     // set the dimensions and margins of the graph
     var margin = { top: 20, right: 20, bottom: 50, left: 70 },
@@ -28,11 +43,11 @@ function LineGraph() {
         .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
     // add X axis and Y axis
-    var x = d3.scaleTime().range([0, width]);
+    var x = d3.scaleLinear().range([0, width]);
     var y = d3.scaleLinear().range([height, 0]);
 
-    x.domain(d3.extent(data, (d) => { return d.date; }));
-    y.domain([0, d3.max(data, (d) => { return d.value; })]);
+    x.domain(d3.extent(data, (help) => { return help.date; }));
+    y.domain([0, d3.max(data, (help) => { return help.value; })]);
   
     svg.append("g")
       .attr("transform", `translate(0, ${height})`)
@@ -43,16 +58,17 @@ function LineGraph() {
       
     // add the Line
     var valueLine = d3.line()
-    .x((d) => { return x(d.date); })
-    .y((d) => { return y(d.value); });
-  
+    .x((help) => { return x(help.date); })
+    .y((help) => { return y(help.value); });
+    console.log(valueLine)
+
     svg.append("path")
       .data([data])
       .attr("class", "line")
       .attr("fill", "none")
       .attr("stroke", "steelblue")
       .attr("stroke-width", 1.5)
-      .attr("d", valueLine);
+      .attr("help", valueLine);
     svg.append("text")
       .attr("class", "x label")
       .attr("text-anchor", "end")
